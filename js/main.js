d59de7fd09d3094a92fa714f002cbb2119dcd8a0
Dima <dima.aluf@gmail.com>
2024-10-25 14:48:58 +0300

    More functionality and styling

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 
 function onBallClick() {
-    console.log("The ball was clicked!");
+    const ball = document.querySelector(".ball");
+
+
+    let currentSize = parseInt(window.getComputedStyle(ball).width);
+
+    let newSize = currentSize + 50;
+
+    ball.style.width = `${newSize}px`;
+    ball.style.height = `${newSize}px`;
+
+    ball.textContent = newSize;
 }
